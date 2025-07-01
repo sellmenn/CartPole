0c8140af32f341e96956fa3fa2bd976b81c2e3da
sellmenn <105147577+sellmenn@users.noreply.github.com>
2025-07-02 02:17:59 +0800

    network

--- network.py
+++ network.py
@@ -8,9 +8,9 @@ Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'
 class DQN(nn.Module):
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 64)
-        self.layer2 = nn.Linear(64, 64)
-        self.layer3 = nn.Linear(64, n_actions)
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
